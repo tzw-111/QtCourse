f65bb639a9cb3a531fdde78022e0cb120ea24ec4
tzw-111 <3271934182@qq.com>
2025-11-14 09:49:46 +0800

    finish main ui design

---
+++ Mynotepad/mainwindow.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // ui->statusbar;
+    statusLabel.setMaximumWidth(150);
+    statusLabel.setText("length:" + QString::number(0) + "    lines:" + QString::number(1));
+    ui->statusbar->addPermanentWidget(&statusLabel);
+
+    statusCursorLabel.setMaximumWidth(150);
+    statusCursorLabel.setText("Ln:" + QString::number(0) + "    col:" + QString::number(1));
+    ui->statusbar->addPermanentWidget(&statusCursorLabel);
+
+    QLabel *author = new QLabel(ui->statusbar);
+    author->setText(tr("刘文果"));
+    ui->statusbar->addPermanentWidget(author);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
